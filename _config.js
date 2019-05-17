0682f2547d21b99c04522dfa38b87a29eeaab5de
Rub21 <rub2106@gmail.com>
2019-05-17 11:08:04 -0500

    Update config

--- _config.js
+++ _config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  baseurl: 'http://devseed.com/doc-seed',
-  title: 'Doc Seed',
-  description: 'A quick way to create a documentation site',
-  // attribution: {
-    // role: 'Developed for',
-    // name: 'Generic Company',
-    // logo: 'generic-logo-pos-hor.svg',
-    // url: 'https://developmentseed.org'
-  // },
+  baseurl: 'http://devseed.com/housing-passports-labeling',
+  title: 'Housing Passports Labeling',
+  description: 'A quick way for labeling street view images',
+  /**
+  attribution: {
+    role: 'Developed for',
+    name: 'Generic Company',
+    logo: 'generic-logo-pos-hor.svg',
+    url: 'https://developmentseed.org'
+  },
+  */
   sectionsMeta: {
     's0-introduction': 'Introduction',
-    's1-components': 'Components'
+    's1-building-properties': 'Building properties clasification'
   }
 };
