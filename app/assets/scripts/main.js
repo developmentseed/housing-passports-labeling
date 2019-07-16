28f6d1cedee4bb26dc2378aea5cf3595b5f788e4
Rub21 <rub2106@gmail.com>
2019-07-16 09:43:17 -0500

    add modal

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -11,3 +11,18 @@ menuToggle.addEventListener('click', (e) => {
 window.addEventListener('load', function () {
   document.getElementById(`${window.location.pathname}`).classList.add("highlight");
 });
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
